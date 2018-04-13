16fd220aa0b9c103a62609cde3925ab3316e8b64
Michele Ferretti <mic.ferretti@gmail.com>
2018-04-13 18:27:03 +0200

    major update: now with rotating map and coordinated glitch!

--- bundle.js
+++ bundle.js
@@ -4,13 +4,14 @@ const Glitch = require("image-glitch");
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiY2FydG9taWtlIiwiYSI6IjA3ODMzYzc1NDQ5ZmZhMWY1ZjU4NGEwOGUyN2E3OWEzIn0.1AMo07FLn59Mtc3qSNp94g";
-var start = [-74.5, 40];
-var end = [74.5, 40];
+
+var start = [-0.378342, 51.358062];
+var end = [0.140762, 51.597121];
 angle = 0;
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/cartomike/cjfsjwn4x0hbz2sqc88wpnu7b",
-  center: [-73.994958, 40.719022],
+  center: [-0.069695, 51.494049],
   zoom: 12,
   pitch: 60,
   bearing: 0,
@@ -35,7 +36,7 @@ let glitcher = () => {
   image.element.style.position = "absolute";
   image.element.style.height = "100%";
   image.element.id = "glitch";
-  window.document.body.appendChild(image.element);
+  document.getElementById("map").appendChild(image.element);
 };
 
 /**
@@ -46,21 +47,35 @@ let getRandomInt = (min, max) => {
   return Math.random() * (max - min + 1) + min;
 };
 
-map.on("load", function() {
-  glitcher();
-  setInterval(() => {
-    newLng = getRandomInt(-74.014549, -73.972492);
-    newLat = getRandomInt(40.704066, 40.761171);
-    newBearing = getRandomInt(0, 360);
-    map.setCenter([newLng, newLat]);
-    map.setBearing(newBearing);
+/**
+ *
+ * @param {int} val
+ * changes map bearing
+ */
+const rotateMap = val => {
+  map.setBearing(val);
+};
 
+map.on("load", function() {
+  let tmpBearing = 0;
+  let rotateInterval = setInterval(function() {
+    tmpBearing += 0.05;
+    rotateMap(tmpBearing);
+  }, 10);
+  // glitch on first location
+  setTimeout(() => glitcher(), 4000);
+  // start random locations
+  setInterval(() => {
+    tmpBearing = 0;
+    newLng = getRandomInt(-0.378342, 0.140762);
+    newLat = getRandomInt(51.358062, 51.597121);
+
+    map.setCenter([newLng, newLat]);
     document.getElementById("glitch")
       ? document.getElementById("glitch").remove()
       : console.log("start");
-    // document.getElementById("glitch").remove()
-    glitcher();
-  }, 6000);
+    setTimeout(() => glitcher(), 8000);
+  }, 10000);
 });
 
 },{"image-glitch":2}],2:[function(require,module,exports){
